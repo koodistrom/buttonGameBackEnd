28ca6d7c2d021edf61a704190d1a613c66ece736
Jaakko <j.i.mantyla@gmail.com>
2020-02-29 15:51:07 +0200

    Cors changed for heroku

--- src/main/java/com/jaakkomantyla/thebutton/CorsConfiguration.java
+++ src/main/java/com/jaakkomantyla/thebutton/CorsConfiguration.java
@@ -23,7 +23,7 @@ public class CorsConfiguration
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowCredentials(true);
+                registry.addMapping("/**").allowedOrigins("https://jm-button-game.herokuapp.com/").allowCredentials(true);
             }
         };
     }
